7072a13f76d08d601da29955bda7b554f4c46ec1
Kabil Ibrahimi <kabili81@hotmail.com>
2022-07-18 13:13:39 +0200

    update

--- src/main.ts
+++ src/main.ts
@@ -17,11 +17,11 @@ let state: State ={
 let appEl = document.querySelector("#app")
 
 function render(){
-    header(appEl)
-    main(appEl)
+    header()
+    main()
 }
 
-function header(appEl : Element){
+function header(){
     
     let headerEl = document.createElement("header")
     
@@ -63,50 +63,47 @@ function header(appEl : Element){
     ulEl.append(userLiEl,shoppingBagLiEl,searchLiEl)
     navEl.append(ulEl)
     headerEl.append(h1El,navEl)
-    appEl.append(headerEl)
+    appEl?.append(headerEl)
     
 }
 
-function main(appEl : Element){
-    // <main>
+function main(){
+ 
     let mainEl = document.createElement("main")
-    //     <nav class="main-nav">
+
     let navEl = document.createElement("nav")
     navEl.className = "main-nav"
-    //       <ul class="main-list">
+   
     let ulEl= document.createElement("ul")
     ulEl.className = "main-list"
-    //         <li class="main-list-item">
+   
     let productsLiEl = document.createElement("li")
     productsLiEl.className = "main-list-item"
-    //           <img class="image" src="https://images.samsung.com/is/image/samsung/p6pim/al/2202/gallery/al-galaxy-a53-5g-a536-sm-a536blbleuc-531392710?$650_519_PNG$" alt="samsung a53" />
+
     let imageEl = document.createElement("img")
     imageEl.className = "image"
     imageEl.src = "https://images.samsung.com/is/image/samsung/p6pim/al/2202/gallery/al-galaxy-a53-5g-a536-sm-a536blbleuc-531392710?$650_519_PNG$"
     imageEl.alt = "samsung a53"
-    //           <h3>samsung</h3>
+ 
     let titleEl = document.createElement("h3")
     titleEl.textContent = "samsung"
-    //           <span>$23,44</span>
+   
     let priceEl = document.createElement("span")
     priceEl.textContent = "$23,44"
    
     let discountEl = document.createElement("span")
     discountEl.textContent = "$19,44"
-    //           <p>samsung</p>
+   
     let desciptionEl = document.createElement("p")
     desciptionEl.textContent = "samsung new phone"
-    //         </li>
-    //       </ul>
-    //     </nav>
-    //   </main>
+  
 
     productsLiEl.append(imageEl,titleEl,priceEl,discountEl,desciptionEl)
     ulEl.append(productsLiEl)
     navEl.append(ulEl)
     mainEl.append(navEl)
-    appEl.append(mainEl)
+    appEl?.append(mainEl)
 }
-renderHeader(appEl)
+
 
 render()
